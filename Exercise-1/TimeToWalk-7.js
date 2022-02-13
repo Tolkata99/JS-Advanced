cb5521438b0fd39b630b58a1f99d1493eba7f342
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2022-02-13 07:51:37 +0200

    I solved ..

--- Exercise-1/TimeToWalk-7.js
+++ Exercise-1/TimeToWalk-7.js
@@ -7,7 +7,7 @@ let time = distance / speed + rest;
 
 let hours = Math.floor(time / 3600).toFixed(0).padStart(2, '0');
 let minutes = Math.floor(time / 60).toFixed(0).padStart(2, '0');
-let seconds = Math.floor(time % 60).toFixed(0) .padStart(2, '0');
+let seconds = Math.floor(time % 60).toFixed(0).padStart(2, '0');
 
 console.log(`${hours}:${minutes}:${seconds}`);
 
