0890e69d8a09e7c6ba64c04b29da3114146b7e1d
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2022-02-28 18:28:35 +0200

    I solved colonize table

---
+++ Lab3-DOM Introduction/Task-5ColorizeTable/ColoriseTable.js
@@ -0,0 +1,19 @@
+function colorise(){
+
+    let rowElements = document.getElementsByTagName('tr');
+
+    let rows = Array.from(rowElements);
+
+rows.forEach((x,i) => {
+
+    if(i % 2 != 0){
+
+        x.style.backgroundColor = 'teal';
+
+    }
+
+});
+
+
+
+}
